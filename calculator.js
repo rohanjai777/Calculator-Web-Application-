fb855da53f342b2c5777205ddc754e9741bdf967
Rohan Arora <rohanjai777@gmail.com>
2019-08-13 18:07:18 +0530

    first commit

---
+++ calculator.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req,res){
+  res.sendFile(__dirname+"/index.html");
+});
+app.post("/",function(req,res){
+  var num1= Number(req.body.num1);
+  var num2 = Number(req.body.num2);
+  var result = num1+num2;
+  res.send("The result is"+result);
+});
+
+app.get("/bmicalculator", function(req,res){
+  res.sendFile(__dirname+"/bmicalculator.html");
+});
+app.post("/bmicalculator",function(req,res){
+  var num1= Number(req.body.val1);
+  var num2 = Number(req.body.val2);
+  var result = num1/(num2*num2);
+  res.send("Your BMI is"+result);
+});
+
+
+
+app.listen(3000, function(){
+  console.log("Server at port 3000");
+});
